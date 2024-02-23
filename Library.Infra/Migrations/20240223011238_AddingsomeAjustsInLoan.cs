a5b95f5e1ee98851cd9e9a0eb0bc448eb970de06
CAIO FABIO <caiofabio250@gmail.com>
2024-02-22 22:19:50 -0300

    adding migration

---
+++ Library.Infra/Migrations/20240223011238_AddingsomeAjustsInLoan.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingsomeAjustsInLoan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentId",
+                table: "Loans",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalValuePaid",
+                table: "Loans",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentId",
+                table: "Loans");
+
+            migrationBuilder.DropColumn(
+                name: "TotalValuePaid",
+                table: "Loans");
+        }
+    }
+}
